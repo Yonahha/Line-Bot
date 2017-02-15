42db828f657a0486c32160ce6707b3f7b088f81f
Yonahha <kritsana.ch.57@ubu.ac.th>
2017-02-15 21:53:04 +0700

    test-yield-2

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function getWeather(){
   });
 }
 
-/* Old
+
 app.post('/webhook', (req, res) => {
   var text = req.body.events[0].message.text
   var sender = req.body.events[0].source.userId
@@ -55,64 +55,25 @@ app.post('/webhook', (req, res) => {
   //console.log(typeof sender, typeof text);
   // console.log(req.body.events[0])
 
-  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+  /*if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
     sendText(sender, text);
-  }
+  }*/
   sendText(sender, text);
   res.sendStatus(200);
 });
-*/
-//new
-app.post('/webhook', (req, res) => {
-    async.waterfall([
-            function(callback) {
-                request({url: 'http://api.wunderground.com/api/e1cb835416fecd99/conditions/q/TH/Ubon_Ratchathani.json', json:true}, function(err, res, json){
-					if (err) {
-						throw err;
-					}
-					callback(json);
-				})
-            },
-        ],
-        function(jsonData) {
-            var headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer {YV4YHUNnJhTURSd9BzLGokn7ALa8+pKl/KooSoAEW7CL4yNF9TwjC3Jw5TuivsoQ3VwhB87kTwCamwcHFHj0Qv6XGMZKbJYXziekYqmHFnBj9AvZpxya3rRNupun8JIFv5EzUZUPlfZcywrvH9jhgQdB04t89/1O/w1cDnyilFU=}'
-            };
-            var data = {
-                'replyToken': req.body['events'][0]['replyToken'],
-                "messages": [{
-                    "type": "text",
-                    "text": jsonData
-                }]
-            };
-            var options = {
-                url: 'https://api.line.me/v2/bot/message/reply',
-                proxy: process.env.FIXIE_URL,
-                headers: headers,
-                json: true,
-                body: data
-            };
 
-            request.post(options, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log(body);
-                } else {
-                    console.log('error: ' + JSON.stringify(response));
-                }
-            });
-        }
-    );
-});
 
 function sendText (sender, text) {
-  var tmp = function(){
-              request({url: 'http://api.wunderground.com/api/ff6d8d1f8d1c171e/conditions/q/TH/Ubon_Ratchathani.json', json:true}, function(err, res, json){
-              if (err) {throw err;}
-              return json;
-            });
-          }
-  //var tmp = yield getWeather();
+  var tmp;
+  request({url: 'http://api.wunderground.com/api/ff6d8d1f8d1c171e/conditions/q/TH/Ubon_Ratchathani.json', json:true}, function(err, res, json){
+    if (err) {
+      throw err;
+    }
+//    var obj = json['current_observation'];
+//    var obj1 = obj["display_location"];
+//    return obj1["state_name"];
+      tmp =  json;
+  });
   console.log(tmp);
   var data = {
     to: sender,
