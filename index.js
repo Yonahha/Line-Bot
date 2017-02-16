097e6f1d82d0049510cdd7742930bd4c7d66f4b4
Yonahha <kritsana.ch.57@ubu.ac.th>
2017-02-16 15:20:14 +0700

    test-weather-0

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/db', function (request, response) {
 });
 
 app.get('/image/*', function(request, response) {
-  response.render(('pages/db', {tmpdata: request});
+  response.render('pages/db', {tmpdata: request});
 });
 
 app.post('/webhook', (req, res) => {
